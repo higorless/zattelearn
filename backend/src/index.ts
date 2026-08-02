12b41590881666e268a1363d868d3dd051a503ca
higorless <higor.fernandes98@hotmail.com>
2026-08-02 13:54:27 -0300

    [FIX] Adding cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,10 @@ import db from './db/knex';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : '*',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(requestLogger);
 
